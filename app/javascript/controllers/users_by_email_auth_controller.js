3d5127da766d8cacc7a1b4e504994de28a5b4471
Angel Aguirre Obando <anngelao25@gmail.com>
2024-06-26 03:37:39 -0500

    Submitting user auth form in modal

---
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus"
+import  axios from "axios"
+
+// Connects to data-controller="users-by-email-auth"
+export default class extends Controller {
+  static targets = ["email", "submit"];
+  connect() {
+    this.submitTarget.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if (this.emailTarget.value.length === 0) {
+        console.log('inside if email field is empty');
+        // email field is empty, so don't do anything
+
+      } else {
+        console.log("inside and email is NOT empty");
+        // email field is filled out, so do something
+        axios.get('/api/users_by_email', {
+          params: {
+            email: this.emailTarget.value,
+          },
+          headers: {
+            "ACCEPT": "application/json" 
+          }
+        }).then((response) => {
+            Turbo.visit('/users/sign_in');
+          }).catch((response) => {
+            Turbo.visit('/users/sign_up');
+          })
+      }
+    });
+     
+  }
+}
