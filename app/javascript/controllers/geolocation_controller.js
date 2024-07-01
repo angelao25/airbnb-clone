7e67c9e8aa3fbe5d057753c3522ce88b6ab49cc9
Angel <anngelao25@gmail.com>
2024-07-01 15:57:20 -0500

    Added javascript to calculate distance when user is not logged in

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,14 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
+import { getDistance, convertDistance } from "geolib"
 
 // Connects to data-controller="geolocation"
 export default class extends Controller {
+  static targets = ["property"];
   connect() {
     window.navigator.geolocation.getCurrentPosition((position) => {
-      // console.log(pos);
-      // var lat = pos.coords.latitude;
-      // var lon = pos.coords.longitude; 
       this.element.dataset.latitude = position.coords.latitude;
       this.element.dataset.longitude = position.coords.longitude;
+
+      console.log("This is the current latitude of your location: ", position.coords.latitude);
+      console.log("This is the current longitude of your location: ", position.coords.longitude);
+
+      this.propertyTargets.forEach((propertyTarget) => {
+        let distanceFrom = getDistance(
+          { latitude: position.coords.latitude, longitude: position.coords.longitude },
+          { latitude: propertyTarget.dataset.latitude, longitude: propertyTarget.dataset.longitude },
+        );
+
+        propertyTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom, 'km')) } kilometers away`;
+      });
     });
   }
 }
